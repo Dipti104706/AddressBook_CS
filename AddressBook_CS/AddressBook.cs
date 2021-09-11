1a024f5835e637367fa6fe13bd120468376ea08c
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-11 18:39:52 +0530

    [Dipti] added code to show welcome note

---
+++ AddressBook_CS/AddressBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_CS
+{
+    class AddressBook
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            Console.ReadLine();
+        }
+    }
+}
